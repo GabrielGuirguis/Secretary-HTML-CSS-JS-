608b2677b0574491f58f41b754b512b7157b07fd
Gabriel Guirguis <123414705+GabrielGuirguis@users.noreply.github.com>
2025-06-09 16:08:36 -0400

    Restyled extension, linked popup with background

--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
 import { convertTo12Hour } from "./utils/convertTo12Hour.js";
 
+var event = true;
+
+const dateDisplay = document.getElementById("date-display")
+const datePicker = document.getElementById("date-picker")
+const startDisplay = document.getElementById("start-display")
+const endDisplay = document.getElementById("end-display")
+const startPicker = document.getElementById("start-picker")
+const endPicker = document.getElementById("end-picker")
+const eventIndicator = document.getElementById("event-indicator")
+const taskIndicator = document.getElementById("task-indicator")
+
+dateDisplay.textContent = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+})
+
 document.getElementById("submitForm").addEventListener("submit", (e) => {
     e.preventDefault()
     
@@ -8,8 +26,8 @@ document.getElementById("submitForm").addEventListener("submit", (e) => {
 
         "summary": document.getElementById("summary").value,
         "description": document.getElementById("description").value,
-        "start": document.getElementById("start").value,
-        "end": document.getElementById("end").value,
+        "start": datePicker.value + `T` + startPicker.value,
+        "end": datePicker.value + `T` + endPicker.value,
 
     }, (response) => {
         if (response.success) {
@@ -21,40 +39,38 @@ document.getElementById("submitForm").addEventListener("submit", (e) => {
     })
 })
 
-const eventIndicator = document.getElementById("event-indicator")
-const taskIndicator = document.getElementById("task-indicator")
+
 
 document.getElementById("event").addEventListener("click", () => {
     eventIndicator.classList.add("selected")
     taskIndicator.classList.remove("selected")
+    event = true;
 })
 
 document.getElementById("task").addEventListener("click", () => {
     taskIndicator.classList.add("selected")
     eventIndicator.classList.remove("selected")
+    event = false;
 })
-
-const dateDisplay = document.getElementById("date-display")
-const datePicker = document.getElementById("date-picker")
 
 dateDisplay.addEventListener("click", () => {
     datePicker.showPicker();
 })
 
 datePicker.addEventListener("change", (e) => {
-    const date = e.target.value
-    dateDisplay.textContent = new Date(date).toLocaleDateString("en-US", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        year: "numeric"
+    const [year, month, day] = e.target.value.split("-").map(Number)
+    const localDate = new Date(year, month - 1, day)
+  
+    dateDisplay.textContent = localDate.toLocaleDateString("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric"
     })
+
 })
 
-const startDisplay = document.getElementById("start-display")
-const startPicker = document.getElementById("start-picker")
-const endDisplay = document.getElementById("end-display")
-const endPicker = document.getElementById("end-picker")
+
 
 startDisplay.addEventListener("click", () => {
     startPicker.showPicker();
